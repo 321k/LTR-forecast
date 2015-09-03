868e72cc777a3fea1b903c334a9d3e6f9c0aa82a
321k <erik.johansson@transferwise.com>
2015-09-03 13:48:52 +0100

    First iteration ready

--- ols_model.R
+++ ols_model.R
@@ -1,3 +1,5 @@
+setwd('/Users/transferwise/LTR-forecast')
+
 library(data.table)
 library(tseries)
 library(sqldf)
@@ -5,7 +7,8 @@ library(ggplot2)
 library(plyr)
 library(quantmod)
 library(reshape2)
-setwd('/Users/transferwise/LTR-forecast')
+source('functions.R')
+
 db <- db_d <- fread('Data/ready_data.csv', data.table=F, stringsAsFactors=T)
 #load('Data/ready_data.RData') #  Data frame is named ltr
 #db <- ltr
@@ -26,15 +29,15 @@ for(i in c('attr_category_level_1', 'cntry_first_address')){
 #db$value_p1 <- db$value + 1
 
 # Aggregate data by attribution
-head(db)
-x <- sqldf('select variable, attr_category_level_1, avg(value) as value from db group by 1,2')
-head(x)
-#x$d_value <- Delt(x$value, type='log')[,1]
-x <- ddply(x, "attr_category_level_1", transform,  DeltaCol = Delt(value, type='arithmetic'), .progress='text')
-names(x)[ncol(x)] <- 'd_value'
-ggplot(x, aes(variable, d_value, color=attr_category_level_1))+geom_line()
+#head(db)
+#x <- sqldf('select variable, attr_category_level_1, avg(value) as value from db group by 1,2')
 
-# Aggregate data, no grouping
+#x$d_value <- Delt(x$value, type='log')[,1]
+#x <- ddply(x, "attr_category_level_1", transform,  DeltaCol = Delt(value, type='arithmetic'), .progress='text')
+#names(x)[ncol(x)] <- 'd_value'
+#ggplot(x, aes(variable, d_value, color=attr_category_level_1))+geom_line()
+
+# Table with month on month expected growth
 growth_path <- sqldf('select variable, avg(value) as value from db group by 1')
 growth_path$d_value <- Delt(growth_path$value, type='log')
 growth_path$cum_d_value <- NA
@@ -43,41 +46,45 @@ for(i in 1:n){
   growth_path$cum_d_value[i] <- prod(exp(growth_path$d_value[i:n]))
 }
 
-ggplot(growth, aes(variable, d_value))+geom_line()
 
-users <- sample(unique(db$id_user), 10000)
-r <- which(db$id_user %in% users)
-db_f <- db
+# Old code for creating samples
+#users <- sample(unique(db$id_user), 10000)
+#r <- which(db$id_user %in% users)
 #db_f <- db[r,]
-#tmp <- db
+
+db_f <- db
 db_f <- db_f[c('id_user', 'variable', 'value')]
-db_f_act <- db_f <- dcast(db_f, id_user~variable, fun.aggregate=mean)
+db_f <- dcast(db_f, id_user~variable, fun.aggregate=mean)
+db_f <- db_f[1:19] #  Remove LTR beyond 18 months
 
-start=2
-end=19
-db_f <- db_f[1:19]
-?cast
+db_f$missing <- apply(db_f, 1, function(x) length(which(!is.finite(x)))) #  Count number of periods to corecast
+db_f$last_value <- 18 - db_f$missing #  Row containing last actual ltr value
 
-db_f$missing <- apply(db_f, 1, function(x) length(which(!is.finite(x))))
-db_f$last_value <- 18 - db_f$missing
-db_f$g18 <- apply(db_f, 1, function(x) cumgrowth(x, growth_path))
-head(db_f)
+db_f$ltr_18_month_forecast <- apply(db_f, 1, function(x) cumgrowth(x, growth_path)) #  Forecast LTR
+#db_f$ltr_17_month_forecast <- apply(db_f, 1, function(x) cumgrowth(x, growth_path, end=17)) #  Forecast LTR
+#db_f$ltr_6_month_forecast <- apply(db_f, 1, function(x) cumgrowth(x, growth_path, end=6)) #  Forecast LTR
 
-x <- (db_f[100000:(100000+2),])
-x
-cumgrowth <- function(x, growth_path){
-  if(x[21]==18){
-    return(x[19])
-  }
-  i_last_ltr <- (x[21])+1
-  i_growth_start <- (x[21])+1
-  growth <- growth_path$cum_d_value[i_growth_start]
-  growth_f <- x[i_last_ltr] * growth
-  return(growth_f)
+write.csv(db_f, 'Data/forecast.csv')
+
+
+for_upload1 <- for_upload[1:100000,]
+for_upload2 <- for_upload[nrow(),]
+for_upload3
+for_upload4
+
+db_upload <- db_f[c('id_user', 'ltr_18_month_forecast')]
+
+steps=100000
+counter=1
+check <- 0
+for(i in seq(1, nrow(db_f), by=steps)){
+  for_upload <- (db_f[i:min(nrow(db_f),(i+steps-1)), c('id_user', 'ltr_18_month_forecast')])
+  print(paste(counter, nrow(for_upload), sep=', '))
+  write.table(for_upload, paste('Data/for_upload', counter, '.csv', sep=""), sep=',', na='NULL', row.names=FALSE)
+  counter <- counter+1
+  check <- check + nrow(for_upload)
 }
 
+nrow(db_f) - check
 
-db_f[280:300,-c(1:6)]
-db[200:500,]
-growth_path
 
