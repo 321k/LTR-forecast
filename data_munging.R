bc9b821d1c9c87f4ee6095ee5dfe225ec4e9b349
321k <erik.johansson@transferwise.com>
2015-09-08 18:07:51 -0400

    Cleaned up code

--- data_munging.R
+++ data_munging.R
@@ -11,15 +11,22 @@ db$attr_category_level_1[which(is.na(db$attr_category_level_1))]='Unknown'
 db$cntry_first_address[which(is.na(db$cntry_first_address))]='Unknown'
 fees_incr <- 32:55
 db[fees_incr] <- apply(db[fees_incr], 2, as.numeric)
-include <- names(db)[c(18,1,8, fees_incr)]
+measures <- c('id_user','first_successful_payment', 'attr_category_level_1','cntry_first_address','first_ccy_target')
+include <- c(measures, names(db)[fees_incr])
 
 ltr <- db[include]
-
-ltr <- melt(ltr, na.rm=T, id.vars=include[1:3])
+ltr <- melt(ltr, na.rm=T, id.vars=measures)
 sum(ltr$value) - sum(colSums(db[fees_incr], na.rm=T)) #  The total sum stays intact
 ltr$variable <- as.character(ltr$variable)
 ltr$variable <- as.numeric(substr(ltr$variable, 12, 13))
 ltr <- ltr[order(ltr$variable),]
 ltr <- ltr[order(ltr$id_user),]
+ltr$first_successful_payment <- as.Date(ltr$first_successful_payment, '%Y-%m-%d')
+ltr$first_successful_payment_year <- year(ltr$first_successful_payment)
+
+n <- nrow(ltr)
+prev_value <- c(NA, ltr$value[-n])
+prev_value[which(ltr$variable==1)] = NA
+ltr$d_value <- log(ltr$value/prev_value)
 
 write.csv(ltr, 'Data/ready_data.csv')
