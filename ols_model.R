a57db70fd8bd6eac50046639403bba48067a4e57
321k <erik.johansson@transferwise.com>
2015-09-08 18:07:37 -0400

    Cleaned up code

--- ols_model.R
+++ ols_model.R
@@ -8,25 +8,44 @@ library(plyr)
 library(quantmod)
 library(reshape2)
 library(Kmisc)
+library(foreach)
 source('functions.R')
 
 db <- db_d <- fread('Data/ready_data.csv', data.table=F, stringsAsFactors=T)
 db[,'value'] <- as.numeric(db[,'value'])
 
-head(db)
-for(i in c('attr_category_level_1', 'cntry_first_address')){
+# The measures variable should always be the same as in the data_munging script
+#measures <- c('id_user','first_successful_payment', 'attr_category_level_1','cntry_first_address','first_ccy_target')
+
+for(i in measures){
   db[,i]  <- as.factor(db[,i])
 }
 
+db$id_user <- as.numeric(as.character(db$id_user))
 
-# Table with month on month expected growth
+
+### Table with month on month expected growth
+#growth_path <- sqldf('select variable, avg(value) as value, avg(d_value) as d_value from db group by 1')
+#growth_path <- sqldf('select variable, cntry_first_address, avg(value) as value from db group by 1,2')
+#growth_path <- ddply(growth_path, "cntry_first_address", transform,  DeltaCol = Delt(value, type='log'))
+#names(growth_path)[ncol(growth_path)] <- 'd_value'
+#ggplot(growth_path, aes(variable, d_value, color=first_ccy_target))+geom_line()
+
 growth_path <- sqldf('select variable, avg(value) as value from db group by 1')
 growth_path$d_value <- Delt(growth_path$value, type='log')
 
-fit <- loess(growth_path$d_value~c(1:24))
-growth_path$loess <- c(NA, fit$fitted)
-
-n <- nrow(growth_path)
+x <- 1:length(growth_path$d_value)
+plot(growth_path$d_value~x)
+r <- 1:16
+y <- growth_path$d_value[r]
+x <- r
+fit <- lm(y~poly(x,2,raw=TRUE))
+xx <- 1:14
+lines(xx, predict(fit, data.frame(x=xx)), col="red")
+pred <- predict(fit, data.frame(x=xx))
+growth_path$pred <- NA
+growth_path$pred[1:length(pred)] <- pred
+growth_path$pred[which(is.na(growth_path$pred))] = min(pred)
 
 
 db_f <- db
@@ -37,7 +56,7 @@ db_f <- db_f[1:19] #  Remove LTR beyond 18 months
 db_f$missing <- apply(db_f, 1, function(x) length(which(!is.finite(x)))) #  Count number of periods to corecast
 db_f$last_value <- 18 - db_f$missing #  Row containing last actual ltr value
 
-db_f$ltr_18_month_forecast <- apply(db_f, 1, function(x) cumgrowth(x, growth_path$d_value)) #  Forecast LTR
+db_f$ltr_18_month_forecast <- apply(db_f, 1, function(x) cumgrowth(x, growth_path$pred)) #  Forecast LTR
 
 write.csv(db_f, 'Data/forecast.csv')
 
@@ -52,3 +71,8 @@ for(i in seq(1, nrow(db_f), by=steps)){
   check <- check + nrow(for_upload)
 }
 
+
+d = data.table(db)
+d <- d[,.N, by=.(id_user, attr_category_level_1)]
+d_s <- d[attr_category_level_1=='Online Performance']
+users <- as.data.frame(d_s[,id_user])
